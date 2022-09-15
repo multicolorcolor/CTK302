518aac516164eb5714d37a04b6ce4780c72ab26c
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2022-09-14 19:39:54 -0500

    sound

---
+++ Unit1/sound/sketch.js
@@ -0,0 +1,30 @@
+let museum;
+
+function preload(){
+  museum = loadSound("assets/museum.mp3");
+}
+function setup() {
+  createCanvas(500, 500);
+  //TextAlign(CENTER);
+  museum.isPlaying();
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  text("first attempt at making sound for unity project", 100, 100, 400, 400);
+
+}
+
+function mouseReleased(){
+  if (museum.isPlaying()){
+    museum.pause();
+  }
+  else{
+    museum.play();
+  }
+}
+
+function touchStarted(){
+  getAudioContext().resume();
+}
