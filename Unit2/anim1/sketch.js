b2f954459096a573890e24c482bdd51047f39e98
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2022-10-12 21:24:44 -0500

    anim

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -1,15 +1,18 @@
 var x = 0;
+let f1;
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("typewriter.ttf");
 }
 
 function draw() {
   background("gray");
   fill("black")
   textSize(128);
-text("Are you here?", x, 100);
+  textFont(f1, 24);
+text("suoq buollun serii/no war", x, 100);
 x += 2;
 if (x > width) {
-  x = -650;
+  x = -300;
 }
 }
