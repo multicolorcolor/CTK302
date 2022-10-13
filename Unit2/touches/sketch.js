a15a95c04273f9b5585eb748ae6fb4f907b52e33
multicolorcolor <112100252+multicolorcolor@users.noreply.github.com>
2022-10-12 21:04:46 -0500

    touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,7 +1,16 @@
 let numberOfTouches ;
+let i1, i2, i3, i4;
 
 function setup() {
   createCanvas(400, 400);
+  //rectMode(CENTER);
+}
+
+function preload(){
+  i1 = loadImage("01.jpg");
+  i2 = loadImage("02.jpg");
+  i3 = loadImage("03.jpg");
+  i4 = loadImage("04.jpg");
 }
 
 function draw() {
@@ -11,22 +20,35 @@ function draw() {
   
   switch(numberOfTouches) {
     case 0: 
-      text("no one is touching the screen", 5, 22) ; 
+    background("grey");
+    textAlign(CENTER);
+    textStyle(BOLD);
+      text("5PM", 200, 50) ; 
+      image(i1, 125, 100, 150, 250);
       break ;
       
       case 1: 
-       text("it's kind of lonely here", 5, 22) ; 
-      // put a picture here
+      background("grey");
+      textAlign(CENTER);
+      textStyle(BOLD);
+       text("12AM", 5, 22) ; 
+       image(i2, 125, 100, 150, 250);
       break ;
       
       case 2:
-      text("two fingers are touching the screen", 5, 22) ; 
-            // put a picture here
+        background("grey");
+        textAlign(CENTER);
+        textStyle(BOLD);
+      text("4AM", 5, 22) ; 
+      image(i3, 125, 100, 150, 250);
       break ;
       
       case 3:
-     text("three are touching the screen", 5, 22) ; 
-            // put a picture here
+        background("grey");
+        textAlign(CENTER);
+        textStyle(BOLD);
+     text("TAKE A BREAK", 5, 22) ; 
+     image(i4, 125, 100, 150, 250);
       break ;
     
       
